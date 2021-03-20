46a37b0ffae5f05dcaec39c5f619c3326abb6401
Claure <cclaurev@est.emi.edu.bo>
2021-03-20 18:48:26 -0400

    Creacion del formulario para corporacion, adicion de tablas en todos los formulacios, conexion con la base de datos y envio de las tablas a los formularios

--- App.py
+++ App.py
@@ -1,18 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2
+
+conexion = psycopg2.connect(host = "tuffi.db.elephantsql.com", database = "ewayqilc", user = "ewayqilc", password = "c_3fHeJM-wn6440Q4ZU0F9hf7dyDu9ba")
 
 app = Flask(__name__)
 
 @app.route('/')
-def Index():
+def index():
     return render_template('index.html')
 
-@app.route('/hangar')
+@app.route('/hangar/')
 def hangares():
-    return render_template('hangar.html')
+    cursor = conexion.cursor()
+    query = "select * from clase_hangar"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('hangar.html', table = data)
 
 @app.route('/persona')
 def personas():
-    return render_template('persona.html')
+    cursor = conexion.cursor()
+    query = "select * from persona"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('persona.html', table = data)
+
+@app.route('/corporacion')
+def corporacion():
+    cursor = conexion.cursor()
+    query = "select * from corporacion"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('corporacion.html', table = data)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
