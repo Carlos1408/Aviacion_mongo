f086b2f31b4bb108f2ccc8750254d677186b099f
Claure <cclaurev@est.emi.edu.bo>
2021-03-21 00:13:28 -0400

    implementacion de la funcion de insercion de datos en los formulacios de persona, hangar y corporacion

--- App.py
+++ App.py
@@ -36,5 +36,41 @@ def corporacion():
     cursor.close()
     return render_template('corporacion.html', table = data)
 
+@app.route("/add-hangar", methods = ['POST'])
+def add_hangar():
+    num_hangar = request.form['num_hangar']
+    capacidad = request.form['capacidad']
+    query = f"""insert into clase_hangar
+                values({num_hangar}, {capacidad})"""
+    cursor = conexion.cursor()
+    cursor.execute(query)
+    cursor.close()
+    return redirect(url_for("hangares"))
+
+@app.route("/add-persona", methods = ['POST'])
+def add_persona():
+    nss = request.form['nss']
+    nombre = request.form['nombre']
+    telefono = request.form['telefono']
+    query = f"""insert into persona (nss, nombre, telefono)
+                values({nss}, '{nombre}', {telefono})"""
+    cursor = conexion.cursor()
+    cursor.execute(query)
+    cursor.close()
+    return redirect(url_for("personas"))
+
+@app.route("/add-corporacion", methods = ['POST'])
+def add_corporacion():
+    nombre = request.form['nombre_corporacion']
+    direccion = request.form['direccion']
+    telefono = request.form['telefono']
+    query = f"""insert into corporacion(nombre, direccion, telefono)
+                values('{nombre}', '{direccion}', {telefono})"""
+    cursor = conexion.cursor()
+    cursor.execute(query)
+    cursor.close()
+
+    return redirect(url_for("corporacion"))
+
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
