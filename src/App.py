82a1f66bfdb4f14205dedd96f43b2d0beeafef08
Claure <cclaurev@est.emi.edu.bo>
2021-04-29 02:12:45 -0400

    implementacion de metodo para recuperar toda la data de los formularios insert y update

--- src/App.py
+++ src/App.py
@@ -25,10 +25,11 @@ def index_register_form(table_name):
 
 @app.route('/insert_register/<schema>/<table_name>', methods = ['POST'])
 def insert_register(schema, table_name):
-    get_form_data(table_name, 'insert')
+    form_data = get_form_data(table_name, 'insert')
+    print(form_data)
     if table_name == 'persona':
         try:
-            return redirect(f"/index_register_form/{request.form['tipo_persona']}")
+            return redirect(f"/index_register_form/persona_{request.form['tipo_persona']}")
         except:
             pass
     return redirect(f"/index-table/{schema}/{table_name}")
@@ -41,12 +42,13 @@ def index_update_form(table_name, data):
     return render_template('index_update_form.html',
                             table = table,
                             data_select = db_data.get_data_select(table_name),
-                            default_values = table['spec_row'][0])
+                            default_values = table['spec_row'][0],
+                            data = data)
 
-@app.route('/update_register/<schema>/<table_name>', methods = ['POST'])
-def update_register(schema, table_name):
-    print('UPDATE REGISTER')
-    get_form_data(table_name, 'update')
+@app.route('/update_register/<schema>/<table_name>/<data>', methods = ['POST'])
+def update_register(schema, table_name, data):
+    form_data = [data] + get_form_data(table_name, 'update')
+    print(form_data)
     return redirect(f"/index-table/{schema}/{table_name}")
 
 # UPDATE REDIRECCION A FORMULARIOS
@@ -67,11 +69,9 @@ def get_form_data(table_name, operation):
     data = []
     table_info = db_data.get_info(table_name)
     for field in bd.select_fields_names(table_info['schema'], table_info['name']):
-        if (operation == 'update' and field[0] != table_info['index']) or (operation == 'insert' and field[0] != 'id'):
-            aux = request.form[field[0]]
-            print(f"{field[0]}: {aux} ", type(aux) )
-            data.append(aux)
-    print('get_form_data: ', str(tuple(data)))
+        if (operation == 'update' and field[0] != table_info['index']) or (operation == 'insert' and (field[0] != 'id' or table_name == 'empleados' or table_name == 'piloto')):
+            data.append(request.form[field[0]])
+    return data
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
--- src/DB_data.py
+++ src/DB_data.py
@@ -4,39 +4,43 @@ class DB_data:
     def __init__(self):
         self.db = DataBase()
         self.tables = {'clase_hangar' : {
-                        'schema' : 'eq',
-                        'name' : 'clase_hangar',
-                        'index' : 'num_hangar'},
-                'persona' : {
-                        'schema' : 'per',
-                        'name' : 'persona',
-                        'index' : 'id'},
-                'empleados' : {
-                        'schema' : 'per',
-                        'name' : 'empleados',
-                        'index' : 'id'},
-                'piloto' : {
-                        'schema' : 'eq',
-                        'name' : 'piloto',
-                        'index' : 'id'},
-                'corporacion' : {
-                        'schema' : 'prop',
-                        'name' : 'corporacion',
-                        'index' : 'nombre'},
-                'servicio' : {
-                        'schema' : 'per',
-                        'name' : 'servicio',
-                        'index' : 'tipo_servicio'},
-                'avion' : {
-                        'schema' : 'eq',
-                        'name' : 'avion',
-                        'index' : 'matricula'},
-                'tipo_avion' : {
-                        'schema' : 'eq',
-                        'name' : 'tipo_avion',
-                        'index' : 'id'}}
+                                'schema' : 'eq',
+                                'name' : 'clase_hangar',
+                                'index' : 'num_hangar'},
+                        'persona' : {
+                                'schema' : 'per',
+                                'name' : 'persona',
+                                'index' : 'id'},
+                        'empleados' : {
+                                'schema' : 'per',
+                                'name' : 'empleados',
+                                'index' : 'id'},
+                        'piloto' : {
+                                'schema' : 'eq',
+                                'name' : 'piloto',
+                                'index' : 'id'},
+                        'corporacion' : {
+                                'schema' : 'prop',
+                                'name' : 'corporacion',
+                                'index' : 'nombre'},
+                        'servicio' : {
+                                'schema' : 'per',
+                                'name' : 'servicio',
+                                'index' : 'tipo_servicio'},
+                        'avion' : {
+                                'schema' : 'eq',
+                                'name' : 'avion',
+                                'index' : 'matricula'},
+                        'tipo_avion' : {
+                                'schema' : 'eq',
+                                'name' : 'tipo_avion',
+                                'index' : 'id'}}
         
     def get_table(self, table_name):
+        if table_name == 'persona_empleados':
+                table_name = 'empleados'
+        elif table_name == 'persona_piloto':
+                table_name = 'piloto'
         table = self.tables[table_name]
         table['fields'] = self.db.select_fields_names(table['schema'], table['name'])
         table['data'] = self.db.select_all(f"{table['schema']}.{table['name']} order by {table['index']}")
@@ -48,12 +52,27 @@ class DB_data:
         return table
 
     def get_data_select(self, table_name):
-        if table_name == 'avion' or table_name == 'empleados' or table_name == 'servicio':
+        if table_name == 'avion':
                 data_select = {'num_hangar' : self.db.select_fields("num_hangar", "eq.clase_hangar order by num_hangar"),
-                                'piloto' :self. db.execute_query_returning_table("select pi.num_lic, pe.nombre from eq.piloto pi inner join per.persona pe on pi.id = pe.id"),
+                                'piloto' : self. db.execute_query_returning_table("select pi.num_lic, pe.nombre from eq.piloto pi inner join per.persona pe on pi.id = pe.id"),
                                 'corporacion' : self.db.select_fields("nombre", "prop.corporacion"),
-                                'tipo_avion' : self.db.select_fields("id, modelo", "eq.tipo_avion order by tipo_avion"),
-                                'servicio' : self.db.select_fields('tipo_servicio', 'per.servicio')}
+                                'tipo_avion' : self.db.select_fields("id, modelo", "eq.tipo_avion order by tipo_avion")}
+        elif table_name == 'empleados':
+                data_select = {'servicio' : self.db.select_fields('tipo_servicio', 'per.servicio'),
+                                'id' : self.db.execute_query_returning_table("select id, nombre from per.persona where id in(select p.id from per.persona p except(select pi.id from eq.piloto pi union select e.id from per.empleados e)) order by id"),
+                                'generated_id' : ''}
+        elif table_name == 'persona_empleados':
+                data_select = {'servicio' : self.db.select_fields('tipo_servicio', 'per.servicio'),
+                                'id' : self.db.execute_query_returning_table("select id, nombre from per.persona where id in(select p.id from per.persona p except(select pi.id from eq.piloto pi union select e.id from per.empleados e)) order by id"),
+                                'generated_id' : self.db.execute_query_returning_table("select max(id) from per.persona")}
+        elif table_name == 'piloto':
+                data_select = {'id' : self.db.execute_query_returning_table("select id, nombre from per.persona where id in(select p.id from per.persona p except(select pi.id from eq.piloto pi union select e.id from per.empleados e)) order by id"),
+                                'generated_id' : ''}
+        elif table_name == 'persona_piloto':
+                data_select = {'id' : self.db.execute_query_returning_table("select id, nombre from per.persona where id in(select p.id from per.persona p except(select pi.id from eq.piloto pi union select e.id from per.empleados e)) order by id"),
+                                'generated_id' : self.db.execute_query_returning_table("select max(id) from per.persona")}
+        elif table_name == 'servicio':
+                data_select = {'tipo_avion' : self.db.select_fields("id, modelo", "eq.tipo_avion order by tipo_avion")}
         else:
                 data_select = None
         return data_select
